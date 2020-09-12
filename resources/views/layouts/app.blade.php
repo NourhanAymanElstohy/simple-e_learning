2c2255d4d211e6fbd593da050f440fa1b591286a
nour <nour7anelsto7y@gmail.com>
2020-09-12 11:39:20 +0200

    add profile pic beside name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,6 +54,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <img src="/upload/profile_pic/{{Auth::user()->image}}" style="width:32px; height: 32px; border-radius: 50%" alt="">
                                     {{ Auth::user()->name }}
                                 </a>
 
